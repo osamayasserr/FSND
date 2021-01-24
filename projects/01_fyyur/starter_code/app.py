b539eb5f78d334c7cd4f2b24395455c7b870ba15
Osama Yasser <osamayasserr@gmail.com>
2021-01-24 18:22:36 +0200

    Fyyur: Implement searching and displaying artists

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -90,6 +90,13 @@ class Venue(db.Model):
             'facebook_link': self.facebook_link,
         }
 
+    def format_m(self):
+        return {
+            'venue_id': self.id,
+            'venue_name': self.name,
+            'venue_image_link': self.image_link
+        }
+
     def format_s(self):
         return {
             'id': self.id,
@@ -129,11 +136,17 @@ class Artist(db.Model):
             'facebook_link': self.facebook_link,
         }
 
-    def format_s(self):
+    def format_m(self):
         return {
             'artist_id': self.id,
             'artist_name': self.name,
             'artist_image_link': self.image_link
+        }
+
+    def format_s(self):
+        return {
+            'id': self.id,
+            'name': self.name
         }
 
 
@@ -173,7 +186,6 @@ def venues():
     date = datetime.now()
 
     try:
-
         # Get all the unique locations (city, state) of venues
         locations = Venue.query.with_entities(
             Venue.city.distinct(), Venue.state).all()
@@ -208,7 +220,7 @@ def venues():
         db.session.rollback()
         print(sys.exc_info())
         flash("Something went wrong. Please try again.")
-        return render_template("pages/home.html")
+        redirect(url_for('index'))
 
     finally:
         db.session.close()
@@ -221,7 +233,6 @@ def search_venues():
     date = datetime.now()
 
     try:
-
         # Get the search term and query the database using LIKE
         search_term = request.form.get('search_term', '')
         venues = Venue.query.filter(Venue.name.ilike(f'%{search_term}%')).all()
@@ -231,7 +242,7 @@ def search_venues():
 
         # Loop over the resulting venues
         for venue in venues:
-            venue_id, venue_dict = venue.id, venue.format_l()
+            venue_id, venue_dict = venue.id, venue.format_s()
 
             # Get the number of upcoming shows for that venue
             venue_dict['num_upcoming_shows'] = Show.query.filter(
@@ -247,7 +258,7 @@ def search_venues():
         db.session.rollback()
         print(sys.exc_info())
         flash("Something went wrong. Please try again.")
-        return render_template("pages/home.html")
+        redirect(url_for('index'))
 
     finally:
         db.session.close()
@@ -260,7 +271,6 @@ def show_venue(venue_id):
     form = DeleteVenue()
 
     try:
-
         # If the user clicks the Delete Venue button
         if form.validate_on_submit():
 
@@ -286,7 +296,7 @@ def show_venue(venue_id):
         # Get the needed data from all upcoming shows
         for show in upcoming_shows:
             artist_id = show.artist_id
-            artist_dict = Artist.query.get(artist_id).format_s()
+            artist_dict = Artist.query.get(artist_id).format_m()
             artist_dict['start_time'] = str(show.start_time)
             venue_dict['upcoming_shows'].append(artist_dict)
 
@@ -300,7 +310,7 @@ def show_venue(venue_id):
         # Get the needed data from past shows
         for show in past_shows:
             artist_id = show.artist_id
-            artist_dict = Artist.query.get(artist_id).format_s()
+            artist_dict = Artist.query.get(artist_id).format_m()
             artist_dict['start_time'] = str(show.start_time)
             venue_dict['past_shows'].append(artist_dict)
 
@@ -308,7 +318,7 @@ def show_venue(venue_id):
 
         return render_template('pages/show_venue.html', venue=venue_dict, form=form)
 
-    except:
+    except Exception:
         db.session.rollback()
         print(sys.exc_info())
         flash("Something went wrong. Please try again.")
@@ -329,7 +339,6 @@ def create_venue_form():
 @ app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
     try:
-
         # Get the submitted form data
         data = request.form
         name = data.get('name', '')
@@ -349,11 +358,11 @@ def create_venue_submission():
         flash('Venue ' + request.form['name'] + ' was successfully listed!')
         return render_template('pages/home.html')
 
-    except:
+    except Exception:
         db.session.rollback()
         print(sys.exc_info())
         flash("Something went wrong. Please try again.")
-        return render_template("pages/home.html")
+        return redirect(url_for('index'))
 
     finally:
         db.session.close()
@@ -364,114 +373,115 @@ def create_venue_submission():
 
 @ app.route('/artists')
 def artists():
-    # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
-    return render_template('pages/artists.html', artists=data)
+
+    data = []
+
+    try:
+        # Get all the artists data
+        artists = Artist.query.all()
+        for artist in artists:
+            data.append(artist.format_s())
+
+        return render_template('pages/artists.html', artists=data)
+
+    except Exception:
+        db.session.rollback()
+        print(sys.exc_info())
+        flash("Something went wrong. Please try again.")
+        return redirect(url_for('index'))
+
+    finally:
+        db.session.close()
 
 
 @ app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
-    response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
-    }
-    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+
+    data = {}
+    date = datetime.now()
+
+    try:
+        # Get the search term and query the database using LIKE
+        search_term = request.form.get('search_term', '')
+        venues = Artist.query.filter(
+            Artist.name.ilike(f'%{search_term}%')).all()
+
+        data['count'] = len(venues)
+        data['data'] = []
+
+        # Loop over the resulting venues
+        for venue in venues:
+            venue_id, venue_dict = venue.id, venue.format_s()
+
+            # Get the number of upcoming shows for that venue
+            venue_dict['num_upcoming_shows'] = Show.query.filter(
+                Show.venue_id == venue_id,
+                Show.start_time > date).count()
+
+            data['data'].append(venue_dict)
+
+        return render_template('pages/search_venues.html',
+                               results=data, search_term=search_term)
+
+    except Exception:
+        db.session.rollback()
+        print(sys.exc_info())
+        flash("Something went wrong. Please try again.")
+        redirect(url_for('index'))
+
+    finally:
+        db.session.close()
 
 
 @ app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
-    # shows the venue page with the given venue_id
-    # TODO: replace with real venue data from the venues table, using venue_id
-    data1 = {
-        "id": 4,
-        "name": "Guns N Petals",
-        "genres": ["Rock n Roll"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "326-123-5000",
-        "website": "https://www.gunsnpetalsband.com",
-        "facebook_link": "https://www.facebook.com/GunsNPetals",
-        "seeking_venue": True,
-        "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-        "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-        "past_shows": [{
-            "venue_id": 1,
-            "venue_name": "The Musical Hop",
-            "venue_image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-            "start_time": "2019-05-21T21:30:00.000Z"
-        }],
-        "upcoming_shows": [],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 0,
-    }
-    data2 = {
-        "id": 5,
-        "name": "Matt Quevedo",
-        "genres": ["Jazz"],
-        "city": "New York",
-        "state": "NY",
-        "phone": "300-400-5000",
-        "facebook_link": "https://www.facebook.com/mattquevedo923251523",
-        "seeking_venue": False,
-        "image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-        "past_shows": [{
-            "venue_id": 3,
-            "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-            "start_time": "2019-06-15T23:00:00.000Z"
-        }],
-        "upcoming_shows": [],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 0,
-    }
-    data3 = {
-        "id": 6,
-        "name": "The Wild Sax Band",
-        "genres": ["Jazz", "Classical"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "432-325-5432",
-        "seeking_venue": False,
-        "image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-        "past_shows": [],
-        "upcoming_shows": [{
-            "venue_id": 3,
-            "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-            "start_time": "2035-04-01T20:00:00.000Z"
-        }, {
-            "venue_id": 3,
-            "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-            "start_time": "2035-04-08T20:00:00.000Z"
-        }, {
-            "venue_id": 3,
-            "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-            "start_time": "2035-04-15T20:00:00.000Z"
-        }],
-        "past_shows_count": 0,
-        "upcoming_shows_count": 3,
-    }
-    data = list(filter(lambda d: d['id'] ==
-                       artist_id, [data1, data2, data3]))[0]
-    return render_template('pages/show_artist.html', artist=data)
+
+    date = datetime.now()
+
+    try:
+        # Get the artist with id = artist_id & create a data dict
+        artist_dict = Artist.query.get(artist_id).format_l()
+        artist_dict['upcoming_shows'] = []
+        artist_dict['past_shows'] = []
+
+        # Get the upcoming shows for that artist
+        upcoming_shows = Show.query.filter(
+            Show.artist_id == artist_id,
+            Show.start_time > date).all()
+
+        # Get the needed data from all upcoming shows
+        for show in upcoming_shows:
+            venue_id = show.venue_id
+            venue_dict = Venue.query.get(venue_id).format_m()
+            venue_dict['start_time'] = str(show.start_time)
+            artist_dict['upcoming_shows'].append(venue_dict)
+
+        artist_dict['upcoming_shows_count'] = len(upcoming_shows)
+
+        # Get the past shows for that artist
+        past_shows = Show.query.filter(
+            Show.artist_id == artist_id,
+            Show.start_time < date).all()
+
+        # Get the needed data from past shows
+        for show in past_shows:
+            venue_id = show.venue_id
+            venue_dict = Venue.query.get(venue_id).format_m()
+            venue_dict['start_time'] = str(show.start_time)
+            artist_dict['past_shows'].append(venue_dict)
+
+        artist_dict['past_shows_count'] = len(past_shows)
+
+        return render_template('pages/show_artist.html', artist=artist_dict)
+
+    except Exception:
+        db.session.rollback()
+        print(sys.exc_info())
+        flash("Something went wrong. Please try again.")
+        return redirect(url_for('index'))
+
+    finally:
+        db.session.close()
 
 #  Update
 #  ----------------------------------------------------------------
